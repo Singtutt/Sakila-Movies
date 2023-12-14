60d1187c4c05711def41fe58060cb30a9e14a589
Sing Tuttanon <singtuttanon@gmail.com>
2023-12-14 10:42:18 -0500

    Fixed Pushing Previous Mistake

--- SakilaMovies/src/main/java/com/pluralsight/App.java
+++ SakilaMovies/src/main/java/com/pluralsight/App.java
@@ -7,7 +7,7 @@ public class App {
 
     private static final String URL = "jdbc:mysql://localhost:3306/sakila";
     private static final String USER = "root";
-    private static final String PASS = "1550329";
+    private static final String PASS = "~CONFIDENTIAL~";
 
     public static void main(String[] args) {
         //  Database Information
